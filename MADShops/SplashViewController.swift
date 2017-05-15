ec616e764bf97fd87155140faddd3796eada1e7e
Daniel Ruiz <danruicar@gmail.com>
2017-05-15 09:07:37 +0200

    Added ViewController for splash screen

--- MADShops/SplashViewController.swift
+++ MADShops/SplashViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SplashViewController: UIViewController {
+
+    @IBOutlet weak var activitySpinner: UIActivityIndicatorView!
+    @IBOutlet weak var startButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
