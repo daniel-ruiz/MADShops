2f760aa2b155c342d9f26a702d72dec2b69f5029
Daniel Ruiz <danruicar@gmail.com>
2017-05-15 18:58:23 +0200

    Added content to table view in shop list view controller

--- MADShops/ShopsController.swift
+++ MADShops/ShopsController.swift
@@ -7,29 +7,48 @@
 //
 
 import UIKit
+import CoreData
 
 class ShopsController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    var context = CoreDataStack.sharedInstance.context
+    var cachedShops: [Shop] {
+        get {
+            let request: NSFetchRequest<Shop> = Shop.fetchRequest()
+            let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+            request.fetchBatchSize = 20
+            request.sortDescriptors = [sortDescriptor]
+            do {
+                return try context.fetch(request)
+            } catch {
+                return []
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+
+extension ShopsController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cachedShops.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let shop = cachedShops[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ShopCell", for: indexPath)
+        
+        cell.textLabel?.text = shop.name
+        
+        return cell
     }
-    */
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+}
 
+extension ShopsController: UITableViewDelegate {
+    
 }
